35fe95e0e1360149aec6213fc1f0497996b994fe
AnthDavis26 <42490176+AnthDavis26@users.noreply.github.com>
2019-07-19 19:55:43 -0700

    Initial commit of Planets site

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+	res.render("index.html");
+});
+
+app.get("/mercury", function(req, res){
+	res.render("mercury.html");
+});
+
+app.get("/venus", function(req, res){
+	res.render("venus.html");
+});
+
+// server listener
+app.listen(process.env.PORT, process.env.IP, function(){
+	console.log("Running Express Server..")
+});
