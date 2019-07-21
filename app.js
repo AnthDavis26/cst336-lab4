b0ffcae7bee2dd85dbfd592631919483abd9c2de
AnthDavis26 <42490176+AnthDavis26@users.noreply.github.com>
2019-07-20 21:36:19 -0700

    Hopefully this lab is completed.

--- app.js
+++ app.js
@@ -15,7 +15,42 @@ app.get("/venus", function(req, res){
 	res.render("venus.ejs");
 });
 
-// server listener
+app.get("/earth", function(req, res){
+	res.render("earth.ejs");
+});
+
+app.get("/mars", function(req, res){
+	res.render("mars.ejs");
+});
+
+app.get("/jupiter", function(req, res){
+	res.render("jupiter.ejs");
+});
+
+app.get("/saturn", function(req, res){
+	res.render("saturn.ejs");
+});
+
+app.get("/uranus", function(req, res){
+	res.render("uranus.ejs");
+});
+
+app.get("/neptune", function(req, res){
+	res.render("neptune.ejs");
+});
+
+app.get("/secret", function(req, res){
+	res.render("secret.ejs");
+});
+
+
+// Heroku server listener
 app.listen(process.env.PORT, process.env.IP, function(){
 	console.log("Running Express Server..")
 });
+
+/* Offline testing only
+app.listen("8081", "127.0.0.1", function(){
+	console.log("Running Express Server..")
+});
+*/
